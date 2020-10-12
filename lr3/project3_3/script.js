3c253f30cfb100a6072bf8d4a1ad90dd6f2153ce
Roma <romachne@gmail.com>
2020-10-13 03:21:16 +0500

    lr3p3 rework

--- lr3/project3_3/script.js
+++ lr3/project3_3/script.js
@@ -1,9 +1,14 @@
-var obj = {name: "Roma"};
-var arr = [1, 2, 3, obj];
+var arr = [1, 2, 3, {name: "Roma"}];
 
 function klon(arr) {
-    return arr.slice();
+    return JSON.parse(JSON.stringify(arr));
 }
 
-console.log(klon(arr));
-console.log(klon(arr)[3].name);
+newArr = klon(arr);
+
+console.log('Клонированный массив ' + newArr);
+
+console.log('Старый обьект в клонированном массиве ' + newArr[3].name);
+arr[3].name = "Dima"; // Меняем обьект в старом массиве
+console.log('Обьект в клонированном массиве не меняется ' + newArr[3].name);
+
